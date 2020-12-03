d60b642458a1795de74e443736c30d5747d48389
Phil Mills <pmills@newrelic.com>
2020-12-03 12:20:14 -0800

    Problem #6 solved!

---
+++ 6/main.rb
@@ -0,0 +1,22 @@
+input = File.open("../input/trees_map.txt").readlines.map{|i| i.strip}
+rows = input.count
+columns = input[0].length
+xArray = [1, 1, 1, 1, 2]
+yArray = [1, 3, 5, 7, 1]
+treesFound = [0, 0, 0, 0, 0]
+
+for path in 0..4
+  y = 0
+  for x in (xArray[path]..rows-1).step(xArray[path])
+    y = y + yArray[path]
+    if y >= columns
+      y = y - columns
+    end
+  
+    if input[x][y] === "#"
+      treesFound[path] = treesFound[path] + 1
+    end
+  end
+end
+
+puts treesFound.reject(&:zero?).inject(:*)
