397644d088e8ce243d4b6f85fd50df1ea7cf7c22
Phil Mills <pmills@newrelic.com>
2020-12-03 21:57:52 -0800

    Solutions for Day 4!

---
+++ 7/main.rb
@@ -0,0 +1,28 @@
+rows = File.open("../input/passport_data.txt").readlines.map{|i| i.strip}
+passports = []
+passport = {}
+
+rows.each do |r|
+  if r.length > 0
+    elements = r.split(' ')
+    elements.each do |e|
+      element = e.split(':')
+      passport[element[0]] = element[1]
+    end
+  else
+    passports.push(passport)
+    passport = {}
+  end
+end
+
+validCount = 0
+
+passports.each do |x|
+  if x.count === 8
+    validCount = validCount + 1
+  elsif x.count === 7 && !x.key?("cid")
+    validCount = validCount +1
+  end
+end
+
+puts validCount
