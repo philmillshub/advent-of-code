1a4b2ffc0097791d7ee56fdfd1d9b8d64a2194e2
Phil Mills <pmills@newrelic.com>
2020-12-10 09:58:38 -0800

    wip part1 (order of adapters array done)

--- 10/part1.rb
+++ 10/part1.rb
@@ -2,6 +2,8 @@ rows = File.open("input.txt").readlines.map{|i| i.strip.to_i }
 builtIn = rows.max + 3
 order = []
 lastPick = 0
+
+jumps = { "1": 0, "2": 0, "3": 0 }
 loop do
   for i in 0..rows.count-1
     for j in 1..3
@@ -10,6 +12,7 @@ loop do
         found = true
         lastPick = rows[i]
         order.push lastPick
+        jumps[j] += 1
         break
       end
       break if found
