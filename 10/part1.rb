8405c307e0e8d175c53c77851ec879c2f5d46a48
Phil Mills <pmills@newrelic.com>
2020-12-10 10:12:56 -0800

    solution to day 10 part 1

--- 10/part1.rb
+++ 10/part1.rb
@@ -1,22 +1,8 @@
-rows = File.open("input.txt").readlines.map{|i| i.strip.to_i }
-builtIn = rows.max + 3
-order = []
-lastPick = 0
-
-jumps = { "1": 0, "2": 0, "3": 0 }
-loop do
-  for i in 0..rows.count-1
-    for j in 1..3
-      found = false
-      if rows[i] - lastPick === j
-        found = true
-        lastPick = rows[i]
-        order.push lastPick
-        jumps[j] += 1
-        break
-      end
-      break if found
-    end
-  end
-  break if (lastPick + 3) == builtIn
+rows = File.open("input.txt").readlines.map{|i| i.strip.to_i }.sort
+jumps = [0, 0, 0]
+rows.each_with_index do |r, idx|
+  diff = 
+  jumps[(r - (idx > 0 ? rows[idx-1] : 0))-1] += 1
 end
+
+puts jumps[0]*(jumps[2]+1) # +1 for the last jump to the device
