a43c32c57d2ad58bf229ea671411c97c04efff15
Phil Mills <pmills@newrelic.com>
2021-12-09 12:51:02 -0800

    Day 9 Part 1

--- 2021/9/part1.rb
+++ 2021/9/part1.rb
@@ -3,13 +3,14 @@ input = File.open("input.txt").readlines.map{|i| i.strip.chars.map{|j| j.to_i}}
 lowPoints = []
 for row in 0..input.count-1
   for col in 0..input[row].count-1
+    next if input[row][col] === 9
     neighbors = []
     neighbors.push(input[row][col-1]) unless col === 0
     neighbors.push(input[row][col+1]) unless col === input[row].count-1
     neighbors.push(input[row-1][col]) unless row === 0
     neighbors.push(input[row+1][col]) unless row === input.count-1
-    lowPoints.push(input[row][col]neighbors.any? { |n| n < val }
-    break
+    lowPoints.push(input[row][col]) unless neighbors.any? { |n| n < input[row][col] }
   end
-  break
 end
+
+puts lowPoints.sum + lowPoints.count
